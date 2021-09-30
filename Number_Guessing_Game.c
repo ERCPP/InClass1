66b53fe3d6549902759e92e7ce02da509a655ca8
ERCPP <89431024+ERCPP@users.noreply.github.com>
2021-09-29 19:38:29 -0700

    Added outlines to code

---
+++ Number_Guessing_Game.c
@@ -0,0 +1,15 @@
+//Begin loop
+//create random number between 1 and 10
+//Print options to the screen.
+//Get char inputs
+//if input 1
+//start game loop
+//ask user for number and scan for input
+//if input equal, congradulate and break
+//if input q, break
+//else, give high/low notification
+//end game loop
+//if input 2, ask user for new number
+//update the random number variable with one within the specified range.
+//if input is 3, break
+//End Loop
---
+++ Restaraunt_Bill.c
@@ -0,0 +1,10 @@
+//Begin main function
+//Create Arrray With Prices
+//Set a variable to a random number between 0 and 3
+//Use that variable to select the price from the corresponding spot in the array.
+//Ask the user what the tax rate is and scan for input
+//Ask the user what the tip percent is and scan for input
+//Create tax variable and set it equal to the first argument multiplied by the price
+//Create tip variable and set it equal to the second argument multiplied by the price
+//Create total variable and add the original price, the tax, and the tip to it.
+//Print all info to the screen
