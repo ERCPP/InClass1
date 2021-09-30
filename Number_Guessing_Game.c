990ee13a776895a044d269bd2cabfd7209c96fce
ERCPP <89431024+ERCPP@users.noreply.github.com>
2021-09-29 21:04:06 -0700

    Main 2 Programs Completed

--- Number_Guessing_Game.c
+++ Number_Guessing_Game.c
@@ -1,15 +1,79 @@
-//Begin loop
-//create random number between 1 and 10
-//Print options to the screen.
-//Get char inputs
-//if input 1
-//start game loop
-//ask user for number and scan for input
-//if input equal, congradulate and break
-//if input q, break
-//else, give high/low notification
-//end game loop
-//if input 2, ask user for new number
-//update the random number variable with one within the specified range.
-//if input is 3, break
-//End Loop
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+    //Create quit condition
+    int quit = 0;
+    //Set default max
+    int max = 10;
+    //Begin loop
+    while (quit == 0) {
+        char input;
+        //create random number between 1 and 10
+        time_t t;
+        srand((unsigned) time(&t));
+        int randomNum = rand() % max;
+        //increment random number
+        randomNum += 1;
+        //Print options to the screen.
+        printf("Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+        //Get char inputs
+        input = getchar();
+        //if input 1 start game
+        if (input=='1') {
+            char gameInput[3];
+            int gameOver = 0;
+            //start game loop
+            while (gameOver == 0) {
+                //ask user for number and scan for input
+                printf("Please enter your number or press Q to quit.\n");
+                //flush stdin because my 'gets' keeps catching input from somewhere else. maybe the line aboe it or an enter somewhere.
+                fflush(stdin);
+                gets(gameInput);
+                //if input equal, congratulate and break
+                if (atoi(gameInput) == randomNum) {
+                    printf("Great Job!\n");
+                    gameOver = 1;
+                }
+                //if input q, break
+                else if (gameInput[0] == 'q') {
+                    gameOver = 1;
+                }
+                //else, give high/low notification
+                else {
+                    if (atoi(gameInput) > randomNum) {
+                       printf("Try a lower number.\n");
+                   }
+                   else if (atoi(gameInput) < randomNum) {
+                       printf("Try a higher number.\n");
+                   }
+                }
+            //end game loop
+            }
+        }
+        //if input 2, ask user for new number
+        else if (input=='2') {
+            int maxCandidate;
+            printf("Please enter the maximum number that you would like to use.\n");
+            //update the random number variable with one within the specified range.
+            scanf("%d", &maxCandidate);
+            if (maxCandidate < 1000) {
+                max = maxCandidate;
+                printf("New max of %d successfully set.\n", max);
+            }
+            else if (maxCandidate < 1) {
+                printf("Please enter a number greater than zero.\n");
+            }
+            else {
+                printf("Please enter a number less than 1000.\n");
+            }
+        }
+        //if input is 3, set quit to 1
+        else if (input=='3') {
+            quit = 1;
+            printf("Thanks for playing!\n");
+        }
+        fflush(stdin);
+    }
+}
--- Restaraunt_Bill.c
+++ Restaraunt_Bill.c
@@ -1,10 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 //Begin main function
-//Create Arrray With Prices
-//Set a variable to a random number between 0 and 3
-//Use that variable to select the price from the corresponding spot in the array.
-//Ask the user what the tax rate is and scan for input
-//Ask the user what the tip percent is and scan for input
-//Create tax variable and set it equal to the first argument multiplied by the price
-//Create tip variable and set it equal to the second argument multiplied by the price
-//Create total variable and add the original price, the tax, and the tip to it.
-//Print all info to the screen
+int main () {
+    float input;
+    time_t t;
+    //Create Arrray With Prices
+    float prices[4] = {9.95, 4.55, 13.25, 22.35};
+    //Set a variable to a random number between 0 and 3
+    srand((unsigned) time(&t));
+    int randomMeal = rand() % 4;
+    //Use that variable to select the price from the corresponding spot in the array.
+    float randomPrice = prices[randomMeal];
+    //Create tax variable and set it equal to the first argument multiplied by the price
+    printf("Enter tax as percent.\n");
+    scanf("%f", &input);
+    float tax = randomPrice*(input*.01);
+    //Create tip variable and set it equal to the second argument multiplied by the price
+    printf("Enter tip as percent.\n");
+    scanf("%f", &input);
+    float tip = randomPrice*(input*.01);
+    //Create total variable and add the original price, the tax, and the tip to it.
+    float total = randomPrice+tax+tip;
+    //Print all info to the screen
+    printf("The cost of the meal is $%.2f\nThe tax costs $%.2f\nThe tip amount is $%.2f\nThe total cost of the meal is $%.2f\n", randomPrice, tax, tip, total);
+}
